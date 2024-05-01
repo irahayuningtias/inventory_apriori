8a8d7e9f6ca80fe25bab2b00a7108a40a7b5bf71
irahayuningtias <2031710085@student.polinema.ac.id>
2024-05-02 01:15:51 +0700

    Login & logout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,26 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<section class="vh-100" style="background-color: #508bfc;">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+</head>
+
+<body class="bg-gradient-primary">
     <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
@@ -10,6 +29,7 @@
                 <div class="company-logo">
                     <img src="{{ asset('assets/image/logo-hari-hari.png') }}" >
                 </div>
+                <br>
                 <h3 class="mb-5 font-weight-bold text-center" > INVENTORY MANAGEMENT SYSTEM </h3>
 
                 <form method="POST" action="{{ route('login') }}">
@@ -69,5 +89,19 @@
         </div>
         </div>
     </div>
-</section>
-@endsection
+
+
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('assets/avendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
+    
+</body>
+
+</html>
